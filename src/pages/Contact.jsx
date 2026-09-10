522b17593c5ef42a834f770b4a96342c8a274c2d
atmikapai13 <atmikapai@berkeley.edu>
2026-09-10 09:36:32 -0400

    generated new token

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,6 +12,12 @@ export default function Contact() {
       <p className={styles.contactText}>
         You can share your ideas with me at <a href="mailto:atmikapai13@gmail.com">atmikapai13 [at] gmail [.] com</a>, and find other musings on <a href="https://x.com/ap131999" target="_blank" rel="noreferrer">X</a>.
       </p>
+      <p className={styles.contactText}>
+        If you're not in NYC, still happy to chat! Book some time on my <a href="https://calendar.app.google/LgDoohMegwTQqQ3F9" target="_blank" rel="noreferrer">GCal</a>.
+      </p>
+      <p className={styles.contactText}>
+        My code lives and breathes on <a href="https://github.com/atmikapai13" target="_blank" rel="noreferrer">Github</a>.
+      </p>
     </div>
   )
 }
