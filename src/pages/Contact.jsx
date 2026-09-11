60f864607680172f7587912d238d7a65a597dbfb
atmikapai13 <atmikapai@berkeley.edu>
2026-09-11 13:05:15 -0400

    small changes v2.0

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,22 +5,28 @@ import atmika from '../assets/atmika-scary.png'
 export default function Contact() {
   return (
     <div>
-      <MobileHeader title="Contact" subtitle="tea and chatter" />
+      <MobileHeader title="Contact" subtitle="coffee and chitchat" />
       <div className={styles.contactPhotoWrap}>
         <img src={atmika} alt="Atmika Pai" className={styles.contactPhoto} />
       </div>
       <p className={styles.contactText}>
-        The beauty of living in New York City is that the density of people and ideas offers up serendipitous encounters. To that end, if you are in NYC, I'm always happy to grab coffee in Brooklyn, my abode. But, if you're not in NYC, book some time on my <a href="https://calendar.app.google/LgDoohMegwTQqQ3F9" target="_blank" rel="noreferrer">GCal</a>.
+        The beauty of living in New York City is that the density of people and ideas offers serendipitous encounters. To that end, if you are in NYC, I'm happy to grab a coffee. If you prefer the virtual realm, book some time on my <a href="https://calendar.app.google/LgDoohMegwTQqQ3F9" target="_blank" rel="noreferrer">GCal</a>.
         </p>
       <p className={styles.contactText}>
         = <a href="mailto:atmikapai13@gmail.com">atmikapai13 [at] gmail [.] com</a>
         <br></br>
+         = <a href="https://www.linkedin.com/in/atmikapai/" target="_blank" rel="noreferrer">LinkedIn</a>
+      <br></br>
+      <br></br>
         = <a href="https://x.com/ap131999" target="_blank" rel="noreferrer">Twitter</a>
       <br></br>
+      
         = <a href="https://github.com/atmikapai13" target="_blank" rel="noreferrer">Github</a>
       <br></br>
-        = <a href="https://www.linkedin.com/in/atmikapai/" target="_blank" rel="noreferrer">LinkedIn</a>
+       
+       = <a href="https://open.spotify.com/user/31dcr5jiqh7jasnjs6rulusj6oum" target="_blank" rel="noreferrer">Spotify</a>
       <br></br>
+      
       </p>
     </div>
   )
